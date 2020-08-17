69071d944db13169988440e951e8ae9a2e2a7fbb
sunnu06 <sunnu06444@gmail.com>
2020-08-18 07:57:11 +0800

    GLSL (Vertex shader output color to Fragment shader)

--- Learn_openGL/Learn_openGL.cpp
+++ Learn_openGL/Learn_openGL.cpp
@@ -31,15 +31,23 @@ unsigned int indices[] = {
 const char* vertexShaderSource =
 "#version 330 core                                                \n"
 "layout (location = 0) in vec3 aPos;                              \n"
+
+"out vec4 vertexColor;                                            \n"
+
 "void main(){                                                     \n"
-"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}            \n";
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);             \n"
+"    vertexColor = vec4(0.7, 0.4, 0.1, 1);                          \n"
+"           }            \n";
 
 //fragmentShader in GLSL (Shading Language)
 const char* fragmentShaderSource =
 "#version 330 core                                                \n"
 "out vec4 FragColor;                                              \n"
+
+"in vec4 vertexColor;                                             \n"
+
 "void main(){                                                     \n"
-"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}                   \n";
+"    FragColor = vertexColor;}                                    \n";
 
 void processInput(GLFWwindow* window)
 {
@@ -123,7 +131,7 @@ int main()
     //Linking Vertex Attibutes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0); //Enable vertex attibutes
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //drawn in wireframe mode.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //drawn in wireframe mode.
     
     //Ready engines(keep drawing until stop program)
     while (!glfwWindowShouldClose(window)) //render loop window
